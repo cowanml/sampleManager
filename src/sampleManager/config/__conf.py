8256b47c48b2fbe98aad29af86fb3fd92b0574a1
Matt Cowan <cowan@bnl.gov>
2015-02-26 17:00:29 -0500

    thinking this is actually worse :(

--- src/sampleManager/config/__conf.py
+++ src/sampleManager/config/__conf.py
@@ -5,27 +5,32 @@ import ConfigParser
 from os import path
 
 
-config_path = [
-            '/etc/sampleManager.conf',
-            os.path.expanduser('~/sampleManager.conf'),
-            'dataBroker.conf'
-            ]
+config_path = [os.path.join(*d) for d in (
+        (os.path.expanduser('~'), '.config', 'samplemanager', 'connection.conf'),
+        (os.path.expanduser('~'), '.samplemanager.conf'),
+        ('/etc', 'samplemanager', 'connection.conf'),
+        ('.', 'samplemanager.conf')
+        )]
 
 
 def check_config_file():
     result = False
+
     for f in config_path:
         if path.isfile(f):
             result = True
+
     return result
 
 
 def __loadConfig():
     cf=ConfigParser.SafeConfigParser()
+
     if check_config_file():
         cf.read(config_path)
     else:
         raise IOError('sampleManager configuration file does not exist')
+
     return cf
 
 
