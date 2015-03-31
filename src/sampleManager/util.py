70974ef506112dad0ba913a3372cd966e57de68c
Matt Cowan <cowan@bnl.gov>
2015-03-31 09:58:10 -0400

    added utility function to check for None, empty, or nonexistant key

--- src/sampleManager/util.py
+++ src/sampleManager/util.py
@@ -15,3 +15,24 @@ def get_owner():
     """
 
     return "skinner"
+
+
+def run_on_empty(func, arg_list, arg_dict, key, **kwargs):
+    """
+    Check if 'key' exists in dictionary 'kwargs' and is not None or
+    empty.  If not, run the specified function with the arg list and
+    dictionary specified and return it's return value.
+    
+    return func(*arg_list, **arg_dict) if kwargs['key'] is:
+        None
+        ''
+        or raises KeyError
+    """
+
+    try:
+        if kwargs[key] is None or kwargs[key] == '':
+            return func(*arg_list, **arg_dict)
+
+    except KeyError:
+        return func(*arg_list, **arg_dict)
+
